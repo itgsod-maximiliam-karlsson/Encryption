3bf70bce29418b8d477c2ad5a191567fe6c247a8
Lukas Michanek <michanek.lukas@gmail.com>
2014-04-24 09:07:36 +0200

    Comment out testing

--- encryption.rb
+++ encryption.rb
@@ -1,13 +1,13 @@
 # Returns the integer ordinal of a one character string.
 # Uppercase characters (A-Z) has the range 65 to 90.
-p ' '.ord
-p 'a'.ord
-p 'A'.ord
+# p ' '.ord
+# p 'a'.ord
+# p 'A'.ord
 
 # Converts an integer to the original character.
-p 32.chr
-p 97.chr
-p 65.chr
+# p 32.chr
+# p 97.chr
+# p 65.chr
 
 def encrypt(input, offset = 0)(output)
   # Loop through all characters in the string.
