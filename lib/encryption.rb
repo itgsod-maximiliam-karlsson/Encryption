374dc00e3b39b299aa869e7a1275c86c426a140d
Lukas Michanek <michanek.lukas@gmail.com>
2014-04-24 11:15:09 +0200

    Loop through each character

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -13,6 +13,10 @@ def encrypt(input, offset)
   raise ArgumentError, 'String must not be empty!' if input.empty?
   raise ArgumentError, 'Offset must not be zero!' if offset == 0
   output = ""
+
+  input.each_char do |char|
+  end
+
   output
 end
 
