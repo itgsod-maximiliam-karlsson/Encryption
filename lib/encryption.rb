143a695331915cd47af8b1999adeb6439fbf995f
Lukas Michanek <michanek.lukas@gmail.com>
2014-05-07 18:18:21 +0200

    Add encryption functionality

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -13,19 +13,18 @@ def encrypt(input, offset)
   raise ArgumentError, 'String must not be empty!' if input.empty?
   raise ArgumentError, 'Offset must not be zero!' if offset == 0
 
-  upcase_input = input.upcase
-  output = ""
+  upcase, output = input.upcase, String.new
+  whitespace = [32, 9]
 
-  upcase_input.each_char do |char|
-    char == " " ? output += char : output += offset(char, offset).chr
+  upcase.each_char do |char|
+    whitespace.include?(char.ord) ? output += char : output += offset(char, offset).chr
   end
 
   output
 end
 
-def offset(character, amount)
-  offset_char = character.ord + amount
+def offset(character, offset)
+  offset_char = character.ord + offset
   return offset_char if offset_char <= 90
-
-  (90 - offset_char).abs + 64
+  (offset_char - 90) + 64
 end
