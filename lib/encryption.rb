b27d8b4336c8ffe7536658cff8a97dbb83094a0b
Lukas Michanek <michanek.lukas@gmail.com>
2014-05-07 18:19:24 +0200

    Remove unneeded comments

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,13 +1,3 @@
-# Returns the integer ordinal of a one character string.
-# Uppercase characters (A-Z) has the range 65 to 90.
-# p ' '.ord
-# p 'a'.ord
-# p 'A'.ord
-
-# Converts an integer to the original character.
-# p 32.chr
-# p 97.chr
-# p 65.chr
 
 def encrypt(input, offset)
   raise ArgumentError, 'String must not be empty!' if input.empty?
