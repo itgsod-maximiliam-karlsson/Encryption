d28b58665995cc2e098f78c8d13887f74750bb66
itgsod-maximiliam-karlsson <Maximiliam.Karlsson@elev.it-gymnasiet.se>
2014-06-08 03:17:42 +0200

    I made Lukas proud deleted some unnecessary sheeit. XD now a one-liner hail Lukas.

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,8 +1,1 @@
-def encrypt(input, offset) (output = String.new)
-  (input.empty? ? (raise ArgumentError, 'String must not be empty!') : (offset == 0 ? (raise ArgumentError, 'Offset must not be zero!') : ()))..(input.upcase.each_char do |character| character.eql?(' ') ? output += character : output += offset(character, offset) end)
-  output
-end
-def offset(character, offset) (alphabet = ('A'..'Z').to_a)
-  (char_index = alphabet.index(character))..(offbet = alphabet.rotate(offset))
-  offbet[char_index]
-end
+def encrypt(input, offset) (output = String.new);(input.empty? ? (raise ArgumentError, 'String must not be empty!') : (offset == 0 ? (raise ArgumentError, 'Offset must not be zero!') : ()))..(input.upcase.each_char do |character| character.eql?(' ') ? output += character : output += offset(character, offset) end);output end;def offset(character, offset) (alphabet = ('A'..'Z').to_a);(char_index = alphabet.index(character))..(offbet = alphabet.rotate(offset));offbet[char_index] end
