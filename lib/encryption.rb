b16f3a64e62c8ddba220953242260cdf9966587d
itgsod-maximiliam-karlsson <Maximiliam.Karlsson@elev.it-gymnasiet.se>
2014-06-08 02:53:50 +0200

    Made the code unreadable and just to shorten the code.

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,19 +1,8 @@
-def encrypt(input, offset)
-  raise ArgumentError, 'String must not be empty!' if input.empty?
-  raise ArgumentError, 'Offset must not be zero!' if offset == 0
-
-  output = String.new
-
-  input.upcase.each_char do |character|
-    character.eql?(' ') ? output += character : output += offset(character, offset)
-  end
-
+def encrypt(input, offset) (output = String.new)
+  (input.empty? ? (raise ArgumentError, 'String must not be empty!') : (offset == 0 ? (raise ArgumentError, 'Offset must not be zero!') : ()))..(input.upcase.each_char do |character| character.eql?(' ') ? output += character : output += offset(character, offset) end)
   output
 end
-
-def offset(character, offset)
-  alphabet = ('A'..'Z').to_a
-  char_index = alphabet.index(character)
-  offbet = alphabet.rotate(offset)
+def offset(character, offset) (alphabet = ('A'..'Z').to_a)
+  (char_index = alphabet.index(character))..(offbet = alphabet.rotate(offset))
   offbet[char_index]
 end
